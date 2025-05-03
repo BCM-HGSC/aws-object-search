9d7da2cab4e4723fb9a1d36f307db054157a90c3
Walker Hale IV <walker.hale.iv@gmail.com>
2025-05-03 16:10:06 -0500

    Add integration marker for pytest

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+"""
+This file is used to configure pytest and add custom options or markers.
+It allows for the inclusion of integration tests based on command-line options.
+"""
+
+import pytest
+
+
+def pytest_addoption(parser):
+    """Pytest hook"""
+    parser.addoption(
+        "--run-integration",
+        action="store_true",
+        default=False,
+        help="Run tests marked as integration",
+    )
+
+
+def pytest_collection_modifyitems(config, items):
+    """Pytest hook"""
+    if config.getoption("--run-integration"):
+        # If the option is set, do not skip integration tests
+        return
+    skip_integration = pytest.mark.skip(reason="Need --run-integration option to run")
+    for item in items:
+        if "integration" in item.keywords:
+            item.add_marker(skip_integration)
